cc0b827aacf22e01ba44e954d5e6bb28ffa595a5
harry <harrisongitau3@gmail.com>
2021-03-19 05:56:11 +0300

    Update application instance to log to STDOUT

--- app/__init__.py
+++ app/__init__.py
@@ -29,34 +29,43 @@ def start_ngrok():
 if app.config.get("ENV") == "development" and app.config["START_NGROK"]:
     start_ngrok()
 
-if not app.debug:
+if not app.debug and not app.testing:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD']
+                    )
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'],
-            subject='2fa Failure',
+            subject='Flask 2fa Failure',
             credentials=auth, secure=secure
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/2fa.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('2fa Testing')
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/2fa_flask.log',
+            maxBytes=10240,
+            backupCount=10
+        )
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('2fa Testing')
 
 from app import routes, models, errors
