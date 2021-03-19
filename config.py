c7a25f16b272ae683a59c5db0c574d31870f068b
harry <harrisongitau3@gmail.com>
2021-03-19 14:09:04 +0300

    Remove twilio sendgrid configurations

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ class Config(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
 
     # Heroku logs requirement
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
