dc57f7ed61accfab45e243953cef7585611a4b45
harry <harrisongitau3@gmail.com>
2021-03-19 15:49:10 +0300

    Remove sendgrid configurations

--- app/email.py
+++ app/email.py
@@ -19,7 +19,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[2fa] Reset Your Password',
-               sender=app.config['MAIL_DEFAULT_SENDER'],
+               sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
